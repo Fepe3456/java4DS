6dd53d2d099984d11f0792c52eacf9e231a54789
fede <federico.pedretti@buonarroti.tn.it>
2026-01-26 14:44:41 +0100

    Corretto prjTipiGenericiMensole

--- prjTipiGenericiMensole/src/Libro.java
+++ prjTipiGenericiMensole/src/Libro.java
@@ -1,11 +1,30 @@
-public class Libro extends Oggetto {
+public class Libro implements Shelfable {
 
-    Libro(String nome){
-        super(nome);
+    private String titolo;
+    private String trama;
+    private int anno_pubblicazione;
+
+    Libro(String titolo, String trama, int anno_pubblicazione){
+        this.titolo = titolo;
+        this.anno_pubblicazione = anno_pubblicazione;
+        this.trama = trama;
     }
 
-    public String toString(){
-        return ("Il libro si intitola '" + getTitolo() + "'");
+    @Override
+    public String getNome() {
+        return titolo;
+    }
+    @Override
+    public String getDescrizione() {
+        return ( "\nLibro: \n   Titolo: " + titolo + "\n   Anno pubblicazione: " + anno_pubblicazione + "\n   Trama: " + trama );
+    }
+    @Override
+    public void setNome(String nome) {
+        this.titolo = nome;
+    }
+    @Override
+    public void setDescrizione(String descrizione) {
+        this.trama = descrizione;
     }
 
 }
---
+++ prjTipiGenericiMensole/src/Shelfable.java
@@ -0,0 +1,8 @@
+public interface Shelfable {
+
+    public String getNome();
+    public String getDescrizione();
+    public void setNome(String nome);
+    public void setDescrizione(String descrizione);
+
+}
