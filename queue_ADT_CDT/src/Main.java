741066a71c492ef0af11ddd5faf1181331006e99
federico <federico.pedretti@buonarroti.tn.it>
2026-02-10 18:19:15 +0100

    Aggiornato queue_ADT_CDT

--- queue_ADT_CDT/src/Main.java
+++ queue_ADT_CDT/src/Main.java
@@ -1,6 +1,39 @@
 public class Main {
     public static void main(String[] args) {
 
+        ArrayQueueEfficiente<Integer> queue = new ArrayQueueEfficiente<>(5);
+
+        try{
+            queue.enqueue(1);
+            System.out.println( queue.show() );
+            queue.enqueue(2);
+            System.out.println( queue.show() );
+            queue.enqueue(3);
+            System.out.println( queue.show() );
+            queue.enqueue(4);
+            System.out.println( queue.show() );
+            queue.enqueue(5);
+            System.out.println( queue.show() );
+            queue.dequeue();
+            System.out.println( queue.show() );
+            queue.enqueue(6);
+            System.out.println( queue.show() );
+            queue.dequeue();
+            System.out.println( queue.show() );
+            queue.dequeue();
+            System.out.println( queue.show() );
+            queue.dequeue();
+            System.out.println( queue.show() );
+            queue.dequeue();
+            System.out.println( queue.show() );
+            queue.dequeue();
+        }catch(Exception e){
+            System.out.println( e.getMessage() );
+        }
+
+
+        /*
+
         Queue<Integer> q = new ArrayQueue<>();
 
         try{
@@ -15,6 +48,7 @@ public class Main {
         }catch(Exception e){
             System.out.println( e.getMessage() );
         }
+         */
 
     }
 }
